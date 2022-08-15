9157ced5c00d4c8362a458131e1c8a656fbb7594
Sajad Khazraee Ganjifar <sajadganjifar@gmail.com>
2022-08-15 18:18:15 +0430

    fix styles in mobile scale

--- src/components/crypto/Coin.js
+++ src/components/crypto/Coin.js
@@ -7,12 +7,18 @@ const Coin = ({ image, name, price, symbol, marketCap, priceChanges }) => {
 
     return (
         <div className={styles.container}>
-            <img src={image} />
-            <span className={styles.name}>{name}</span>
-            <span className={styles.symbol}>{symbol.toUpperCase()}</span>
-            <span className={styles.price}>{price.toLocaleString()}$</span>
-            <span className={priceChanges > 0 ? styles.greenPriceChange : styles.redPriceChange}>{priceChanges}</span>
-            <span>{marketCap.toLocaleString()}</span>
+            <div>
+                <img src={image} />
+                <span className={styles.name}>{name}</span>
+            </div>
+            <div>
+                <span className={styles.symbol}>{symbol.toUpperCase()}</span>
+                <span className={styles.price}>{price.toLocaleString()}$</span>
+            </div>
+            <div>
+                <span className={priceChanges > 0 ? styles.greenPriceChange : styles.redPriceChange}>{priceChanges}</span>
+                <span>{marketCap.toLocaleString()}</span>
+            </div>
 
         </div>
     );
